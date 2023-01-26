6bc24e34996c21ff46bc7d421fcb156eda7177d1
David Kelly <davidkelly1507@gmail.com>
2023-01-26 09:59:40 +0000

    Cleaning up code
    Cleaned up code, simplifying functions, and adhering to PEP naming conventions

--- YoutubeVideoDownloader.py
+++ YoutubeVideoDownloader.py
@@ -11,8 +11,13 @@ from pytube import Playlist
 from pytube import YouTube
 from pytube.cli import on_progress
 
+VIDEO_TYPE = 0
+AUDIO_TYPE = 1
+ZIP_BOOL = "1"
+FILE_EXTENSIONS = [["mp4", "mov", "avi", "mkv", "wmv", "webm"], ["mp3", "wav", "aac", "ogg", "wma", "flac", "m4a"]]
 
-def validURL(url):
+
+def valid_url(url):
     pattern = '"playabilityStatus":{"status":"ERROR","reason":"Video unavailable"'
     request = requests.get(url)
     if pattern in request.text or "youtube.com/" not in url:
@@ -20,256 +25,223 @@ def validURL(url):
     return True
 
 
-def getURL():
+def get_url():
     url = ""
-    urlValid = False
-    while not urlValid:
+    url_valid = False
+    while not url_valid:
         url = input("Enter the playlist or video URL: ")
-        if (validURL(url)):
-            urlValid = True
+        if valid_url(url):
+            url_valid = True
         else:
             print("Please enter a valid URL")
     return url
 
 
-def getFileType():
-    fileType = ""
-    intType = 0
-    typeValid = False
-    while not typeValid:
-        fileType = input("Enter 1 for video and 2 for only audio: ")
+def get_file_type():
+    int_type = 0
+    type_valid = False
+    while not type_valid:
+        file_type = input(f"Enter {VIDEO_TYPE} for video and {AUDIO_TYPE} for only audio: ")
         try:
-            intType = int(fileType)
-            if intType == 1 or intType == 2:
-                typeValid = True
+            int_type = int(file_type)
+            if int_type == VIDEO_TYPE or int_type == AUDIO_TYPE:
+                type_valid = True
             else:
                 print("Enter a valid file type")
-        except:
+        except ValueError:
             print("Enter a valid file type")
-    return intType
+    return int_type
 
 
-def getVideoFileExtension():
-    fileExtension = ""
-    extensionValid = False
-    while not extensionValid:
-        fileExtension = input("Enter required file extension: ")
-        if fileExtension == "mp4" or fileExtension == "mov" or fileExtension == "avi" or fileExtension == "mkv" or fileExtension == "wmv" or fileExtension == "webm":
-            extensionValid = True
+def get_file_extension(file_type):
+    file_extension = ""
+    extension_valid = False
+    while not extension_valid:
+        file_extension = input("Enter required file extension: ")
+        if file_type == VIDEO_TYPE and file_extension in FILE_EXTENSIONS[VIDEO_TYPE]:
+            extension_valid = True
+        elif file_type == AUDIO_TYPE and file_extension in FILE_EXTENSIONS[AUDIO_TYPE]:
+            extension_valid = True
         else:
             print("Enter a valid file extension")
-    return fileExtension
+    return file_extension.lower()
 
 
-def getAudioFileExtension():
-    fileExtension = ""
-    extensionValid = False
-    while not extensionValid:
-        fileExtension = input("Enter required file extension: ")
-        if fileExtension == "mp3" or fileExtension == "wav" or fileExtension == "aac" or fileExtension == "ogg" or fileExtension == "wma" or fileExtension == "flac" or fileExtension == "m4a":
-            extensionValid = True
-        else:
-            print("Enter a valid file extension")
-    return fileExtension
+def get_download_folder():
+    base_path = os.path.dirname(os.path.realpath('YoutubeVideoDownloader.py'))
+    download_path = base_path + "\\Download\\"
+    if not os.path.exists(download_path):
+        os.mkdir(download_path)
+    return download_path
 
 
-def getFileExtension(fileType):
-    fileExtension = ""
-    if fileType == 1:
-        fileExtension = getVideoFileExtension()
-    elif fileType == 2:
-        fileExtension = getAudioFileExtension()
-    return fileExtension.lower()
-
-
-def getDownloadFolder():
-    basePath = os.path.dirname(os.path.realpath('YoutubeVideoDownloader.py'))
-    downloadPath = basePath + "\\Download\\"
-    if not os.path.exists(downloadPath):
-        os.mkdir(downloadPath)
-    return downloadPath
-
-
-def makeCurrentDownloadFolder(downloadPath):
+def make_current_download_folder(download_path):
     # Getting the location of the py file and creating the initial directory to store the downloads
-    directoryTime = str(date.today()) + "_" + str(datetime.now().strftime("%H-%M-%S"))
-    dir_path = downloadPath + directoryTime
+    directory_time = str(date.today()) + "_" + str(datetime.now().strftime("%H-%M-%S"))
+    dir_path = download_path + directory_time
     os.mkdir(dir_path)
     return dir_path
 
 
-def filezip(dir_path):
+def zip_folder(directory_path):
     # Zips up the directory and deletes the original directory
-    zip_path = dir_path + ".zip"
-    contents = os.walk(dir_path)
+    zip_path = directory_path + ".zip"
+    contents = os.walk(directory_path)
     zip_file = zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED)
     for root, folders, files in contents:
         for file_name in files:
             absolute_path = os.path.join(root, file_name)
-            relative_path = absolute_path.replace(dir_path + '\\', '')
-            print("Adding '%s' to archive." % absolute_path)
+            relative_path = absolute_path.replace(directory_path + '\\', '')
+            print(f"Adding {absolute_path} to archive.")
             zip_file.write(absolute_path, relative_path)
-    print("'%s' created successfully." % zip_path)
+    print(f"{zip_path} created successfully.")
     zip_file.close()
     # Deletes the original folder
-    shutil.rmtree(dir_path)
+    shutil.rmtree(directory_path)
 
 
-def getZipBool():
-    zipbool = input("Enter 1 to zip the files, anything else to leave files in a folder: ")
-    if zipbool == "1":
+def get_zip_bool():
+    zip_bool = input("Enter 1 to zip the files, anything else to leave files in a folder: ")
+    if zip_bool == ZIP_BOOL:
         return True
     return False
 
 
-def getPlaylistBool(url):
+def get_playlist_bool(url):
     if "/playlist?" in url:
         return True
     return False
 
 
-def getValidPlaylistStart(playlistLength):
-    start = ""
-    intStart = 0
-    startValid = False
-    while not startValid:
-        start = input("Enter a playlist lower bound (Leave empty for no lower bound, or enter a negative number to count backwards from the end): ")
+def get_playlist_start(playlist_length):
+    int_start = 0
+    start_valid = False
+    while not start_valid:
+        start = input(
+            "Enter a playlist lower bound (Leave empty for no lower bound, or enter a negative number to count "
+            "backwards from the end): "
+        )
         if start == "":
-            return 1  
+            return 1
         try:
-            intStart = int(start)
-            if intStart < 0 and (intStart * -1) <= playlistLength:
-                return playlistLength + intStart
-            elif intStart <= playlistLength and intStart > 0:
-                startValid = True
+            int_start = int(start)
+            if int_start < 0 and abs(int_start) <= playlist_length:
+                return playlist_length + int_start + 1
+            elif playlist_length >= int_start > 0:
+                start_valid = True
             else:
                 print("Enter a valid playlist lower bound")
-        except:
+        except ValueError:
             print("Enter a valid playlist lower bound")
-    return intStart
+    return int_start
 
 
-def getValidPlaylistEnd(playlistLength, playlistStart):
-    end = ""
-    intEnd = 0
-    endValid = False
-    while not endValid:
+def get_playlist_end(playlist_length, playlist_start):
+    int_end = 0
+    end_valid = False
+    while not end_valid:
         end = input("Enter a playlist upper bound (Leave empty for no upper bound): ")
         if end == "":
-            return playlistLength
+            return playlist_length
         try:
-            intEnd = int(end)
-            if intEnd <= playlistLength and intEnd >= playlistStart:
-                endValid = True
+            int_end = int(end)
+            if playlist_length >= int_end >= playlist_start:
+                end_valid = True
             else:
                 print("Enter a valid playlist upper bound")
-        except:
+        except ValueError:
             print("Enter a valid playlist upper bound")
-    return intEnd
+    return int_end
 
 
-def getPlaylistRange(playlist):
-    playlistLength = len(playlist)
-    playlistStart = getValidPlaylistStart(playlistLength)
-    playlistEnd = getValidPlaylistEnd(playlistLength, playlistStart)
-    return [playlistStart, playlistEnd]
+def get_playlist_range(playlist):
+    playlist_length = len(playlist)
+    playlist_start = get_playlist_start(playlist_length)
+    playlist_end = get_playlist_end(playlist_length, playlist_start)
+    return [playlist_start, playlist_end]
 
 
-def fileTypeChange(filepath, file_extension):
-    p = Path(filepath)
+def change_file_type(file_path, file_extension):
+    p = Path(file_path)
     p.rename(p.with_suffix(f".{file_extension}"))
 
 
-def printDownloading(video, youtube):
+def print_downloading(video, youtube):
     try:
         print(f"Downloading {youtube.title}")
-    except:
+    except Exception as e:
+        print(e)
         print(f"Downloading {video.title().lower()}")
 
 
-def downloadVideoFormat(video, extension, downloadPath):
-    youtube = YouTube(video, on_progress_callback=on_progress)
-    printDownloading(video, youtube)
-    try:
+def download_with_extension(youtube, file_type, extension, download_path):
+    if file_type == VIDEO_TYPE:
         stream = youtube.streams.filter(file_extension=extension)
-        filePath = stream.get_highest_resolution().download(downloadPath)
-    except:
-        print("Couldn't download the requested format")
-        print("Attempting automated conversion")
-        stream = youtube.streams.get_highest_resolution()
-        filePath = stream.download(downloadPath)
-        fileTypeChange(filePath, extension)
-
-
-def downloadAudioFormat(video, extension, downloadPath):
-    youtube = YouTube(video, on_progress_callback=on_progress)
-    printDownloading(video, youtube)
-    try:
+        stream.get_highest_resolution().download(download_path)
+    elif file_type == AUDIO_TYPE:
         stream = youtube.streams.get_audio_only(extension)
-        filePath = stream.download(downloadPath)
-    except:
-        print("Couldn't download the requested format")
-        print("Attempting automated conversion")
+        stream.download(download_path)
+
+
+def download_without_extension(youtube, file_type, download_path):
+    print("Couldn't download the requested format")
+    print("Attempting automated conversion")
+    file_path = ""
+    if file_type == VIDEO_TYPE:
+        stream = youtube.streams.get_highest_resolution()
+        file_path = stream.download(download_path)
+    elif file_type == AUDIO_TYPE:
         stream = youtube.streams.get_audio_only()
-        filePath = stream.download(downloadPath)
-        fileTypeChange(filePath, extension)
+        file_path = stream.download(download_path)
+    return file_path
 
 
-def downloadPlaylist(playlist, downloadPath):
-    playlistRange = getPlaylistRange(playlist)
-    playlistStart = playlistRange[0] - 1
-    playlistEnd = playlistRange[1] - 1
-    fileType = getFileType()
-    fileExtension = getFileExtension(fileType)
+def download(video, extension, download_path, file_type):
+    youtube = YouTube(video, on_progress_callback=on_progress)
+    print_downloading(video, youtube)
+    try:
+        download_with_extension(youtube, file_type, extension, download_path)
+    except AttributeError:
+        file_path = download_without_extension(youtube, file_type, download_path)
+        change_file_type(file_path, extension)
+
+
+def download_playlist(playlist, download_path):
+    playlist_range = get_playlist_range(playlist)
+    playlist_start = playlist_range[0] - 1
+    playlist_end = playlist_range[1] - 1
+    file_type = get_file_type()
+    file_extension = get_file_extension(file_type)
     threads = []
-    if fileType == 1:
-        if playlistStart == playlistEnd:
-            video = playlist[playlistStart]
-            downloadVideoFormat(video, fileExtension, downloadPath)
-        for x in range(playlistStart, playlistEnd + 1):
-            video = playlist[x]
-            thread = Thread(target=downloadVideoFormat, args=(video, fileExtension, downloadPath))
-            thread.start()
-            threads.append(thread)
-    elif fileType == 2:
-        if playlistStart == playlistEnd:
-            video = playlist[playlistStart]
-            downloadAudioFormat(video, fileExtension, downloadPath)
-
-        for x in range(playlistStart, playlistEnd + 1):
-            video = playlist[x]
-            thread = Thread(target=downloadAudioFormat, args=(video, fileExtension, downloadPath))
-            thread.start()
-            threads.append(thread)
-
+    for x in range(playlist_start, playlist_end + 1):
+        video = playlist[x]
+        thread = Thread(target=download, args=(video, file_extension, download_path, file_type))
+        thread.start()
+        threads.append(thread)
     for thread in threads:
         thread.join()
 
 
-
-def downloadVideo(video, downloadPath):
-    fileType = getFileType()
-    fileExtension = getFileExtension(fileType)
-    if fileType == 1:
-        downloadVideoFormat(video, fileExtension, downloadPath)
-    elif fileType == 2:
-        downloadAudioFormat(video, fileExtension, downloadPath)
+def download_single(video, download_path):
+    file_type = get_file_type()
+    file_extension = get_file_extension(file_type)
+    download(video, file_extension, download_path, file_type)
 
 
 def main():
-    downloadPath = getDownloadFolder()
-    currentDownloadPath = makeCurrentDownloadFolder(downloadPath)
-    url = getURL()
-    playlistBool = getPlaylistBool(url)
-    zipBool = getZipBool()
-    if playlistBool:
+    download_path = get_download_folder()
+    current_download_path = make_current_download_folder(download_path)
+    url = get_url()
+    playlist_bool = get_playlist_bool(url)
+    zip_bool = get_zip_bool()
+    if playlist_bool:
         playlist = Playlist(url)
-        downloadPlaylist(playlist, currentDownloadPath)
+        download_playlist(playlist, current_download_path)
     else:
-        downloadVideo(url, currentDownloadPath)
+        download_single(url, current_download_path)
 
-    if zipBool:
-        filezip(currentDownloadPath)
+    if zip_bool:
+        zip_folder(current_download_path)
 
 
 if __name__ == "__main__":
